bb3f5de1fa50b621b1417f95249a11827db78c63
Cameron Dutro <camertron@gmail.com>
2016-03-20 08:54:23 -0700

    Adjust faraday user agent

--- lib/transifex/config.rb
+++ lib/transifex/config.rb
@@ -3,7 +3,7 @@ require 'transifex/version'
 module Transifex
   module Config
     BASE_URL = ENV['TRANSIFEX_BASE_URL'] || 'https://www.transifex.com'
-    USER_AGENT = "transifex-ruby #{Transifex::VERSION}"
+    USER_AGENT = "transifex-client #{Transifex::VERSION}"
     VALID_OPTIONS = [:username, :password]
 
     attr_accessor *VALID_OPTIONS
